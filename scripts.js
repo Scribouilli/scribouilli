fc875829ab1fe0f00acc2cfe27767fea3bcf8f40
newick <erick@entre-quote.com>
2020-04-15 11:03:29 +0200

    le développement moderne, c'est relou, bisou npm, bundle, blabla

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+/*import { Octokit } from './node_modules/@octokit/rest/dist-web/index.js';
+
+console.log(Octokit);*/
+
 const client_id = "2b4ed9ba835b05f83e2d";
 const destination = "https://daktary-team.github.io/scribouilli";
 const redirect_url = "https://file-moi-les-clefs.herokuapp.com/gh-callback";
@@ -6,10 +10,14 @@ const githubLogin = document.querySelector("#github-login");
 githubLogin.href = `https://github.com/login/oauth/authorize?client_id=${client_id}&scope=public_repo&redirect_uri=${redirect_url}?destination=${destination}`;
 
 const accessToken = new URL(location).searchParams.get("access_token");
+
 if (accessToken) {
-  console.log("connecté t'as vu");
-  location.href = "#create-project";
-} else {
-  console.log("bonjoir");
-  location.href = "#welcome";
+    console.log("connecté t'as vu");
+    location.href = "#create-project";
+
+
+    fetch("https://api.github.com/user", {headers: {Authorization: "token " + accessToken}}).then (r => r.json()).then(result=>console.log(result))
+        } else {
+    console.log("bonjoir");
+    location.href = "#welcome";
 }
