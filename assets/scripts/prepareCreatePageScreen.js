ac5a7edd1932aeedd0b8d8ba403705e9b5dc44e9
newick <erick@entre-quote.com>
2020-06-08 12:04:10 +0200

    des vagues histoires de back tick

--- assets/scripts/prepareCreatePageScreen.js
+++ assets/scripts/prepareCreatePageScreen.js
@@ -1,22 +1,20 @@
 export default function (accessToken, login, origin) {
     const form = document.querySelector("#atelier-create-page form");
-    form.addEventListener("submit", ()=> {
-        const title = form.querySelector("#title").value;
-        const content = form.querySelector("#content").value;
+    form.addEventListener("submit", () => {
+            const title = form.querySelector("#title").value;
+            const content = form.querySelector("#content").value;
 
-        d3.json(`https://api.github.com/repos/${login}/${origin}/contents/${title}.md`, {
-            headers: {Authorization: "token " + accessToken},
-            method: "PUT",
-            body: JSON.stringify(
-                {
-                    message: `création de la page ${title},
-                    content: btoa(
-                        `${content}`
+            d3.json(`https://api.github.com/repos/${login}/${origin}/contents/${title}.md`, {
+                    headers: {Authorization: "token " + accessToken},
+                    method: "PUT",
+                    body: JSON.stringify(
+                        {
+                            message: `création de la page ${title}`,
+                            content: btoa(`${content}`)
+                        }
                     )
                 }
             )
         }
-    });
+    );
 }
-
-
