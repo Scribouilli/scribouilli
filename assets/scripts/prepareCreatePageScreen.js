985a21b9fb5c9178aaf909d923f347f6c4ecabb9
newick <erick@entre-quote.com>
2020-06-12 10:46:58 +0200

    vire les accents et caractères spéciaux du nom du fichier

--- assets/scripts/prepareCreatePageScreen.js
+++ assets/scripts/prepareCreatePageScreen.js
@@ -1,3 +1,11 @@
+function makeFileNameFromTitle (title) {
+    const fileName = title.replace(/\/|#|\?/g, "-") // replace url confusing characters
+        .normalize("NFD").replace(/[\u0300-\u036f]/g, "") // remove accent because GH pages triggers file download
+        +".md";
+
+    return fileName;
+}
+
 export default function (accessToken, login, origin) {
     const form = document.querySelector("#atelier-create-page form");
     form.addEventListener("submit", (event) => {
@@ -5,14 +13,21 @@ export default function (accessToken, login, origin) {
 
             const title = form.querySelector("#title").value;
             const content = form.querySelector("#content").value;
+            const fileName = makeFileNameFromTitle(title);
 
-            d3.json(`https://api.github.com/repos/${login}/${origin}/contents/${title}.md`, {
+            d3.json(`https://api.github.com/repos/${login}/${origin}/contents/${fileName}`, {
                     headers: {Authorization: "token " + accessToken},
                     method: "PUT",
                     body: JSON.stringify(
                         {
                             message: `création de la page ${title}`,
-                            content: btoa(`${content}`)
+                            content: btoa(`
+                            ---\n
+                            title: ${title}\n
+                            ---\n\n
+                            ${content}
+                            `)
+
                         }
                     )
                 }
