f5f6416778f29af10f34132782e78bc2d878ef7a
Yannick François <yannick.francois@beta.gouv.fr>
2023-02-16 23:25:39 +0100

    Ajoute un .
    co-authored-by: DavidBruant <davidbruant@protonmail.com>
    co-authored-by: Booteille <booteille@protonmail.com>

--- assets/scripts/components/Welcome.svelte
+++ assets/scripts/components/Welcome.svelte
@@ -1,6 +1,6 @@
 <section class="screen" id="welcome">
     <h2>Un espace collaboratif <br> de publication de contenu.</h2>
 
-    <a href="/account" class="btn">Créer un espace</a>
+    <a href="./account" class="btn">Créer un espace</a>
     <!-- <a href="#account">Rejoindre un espace</a> -->
 </section>
