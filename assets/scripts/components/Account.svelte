db0975b0470d9329a67a9e04385cafc7492cba60
Yannick François <yannick.francois@beta.gouv.fr>
2023-02-16 23:42:04 +0100

    Ajoute des .
    co-authored-by: DavidBruant <davidbruant@protonmail.com>
    co-authored-by: Booteille <booteille@protonmail.com>

--- assets/scripts/components/Account.svelte
+++ assets/scripts/components/Account.svelte
@@ -1,6 +1,6 @@
 <section class="screen" id="account">
     <h2>Pour pouvoir publier votre contenu, il faut que Scribouilli se connecte à un compte Github.</h2>
 
-    <a href="/login" class="btn">J'ai déjà un compte</a>
+    <a href="./login" class="btn">J'ai déjà un compte</a>
     <!-- <a href="#create-account">Pas de compte</a> -->
 </section>
