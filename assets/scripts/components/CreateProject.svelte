fe4906bde87ea780f10de5c488dc3828031703d1
Yannick François <yannick.francois@beta.gouv.fr>
2023-02-17 13:24:16 +0100

    Revoi la page de création d'un site
    co-authored-by: DavidBruant <davidbruant@protonmail.com>
    co-authored-by: Booteille <booteille@protonmail.com>

--- assets/scripts/components/CreateProject.svelte
+++ assets/scripts/components/CreateProject.svelte
@@ -1,52 +1,57 @@
 <script>
-    export let publishedWebsiteURL;
-    export let createProject;
+  import Skeleton from "./Skeleton.svelte";
 
-    let projectCreationProgressP = undefined;
+  export let publishedWebsiteURL;
+  export let createProject;
 
-    function clickListener(){
-        projectCreationProgressP = Promise.resolve(createProject)
-            .then(createProject => createProject())
-    }
+  let projectCreationProgressP = undefined;
 
+  function clickListener() {
+    projectCreationProgressP = Promise.resolve(createProject).then(
+      (createProject) => createProject()
+    );
+  }
 </script>
 
-{#if projectCreationProgressP === undefined}
+<Skeleton {publishedWebsiteURL}>
+  {#if projectCreationProgressP === undefined}
     <section class="screen" id="create-project">
-        <h2>Créez un projet</h2>
+      <h2>Créez un site</h2>
 
-        {#await publishedWebsiteURL}
+      {#await publishedWebsiteURL}
         (en attente de l'origine)
-        {:then url}
-        <h3>Ce projet va être créé à l'adresse <span class="url">{url}</span> </h3>
-        {/await}    
+      {:then url}
+        <h3>Ce site va être créé à l'adresse <span class="url">{url}</span></h3>
+      {/await}
 
-        <button class="btn submit" on:click={clickListener}>Créer le projet</button>
+      <button class="btn submit" on:click={clickListener}>Créer le site</button>
     </section>
-{:else}
+  {:else}
     {#await projectCreationProgressP}
-
-    <section class="screen" id="loader">
+      <section class="screen" id="loader">
         <h2>Nous sommes en train de créer l'espace de publication</h2>
-        <img src="./assets/images/hearts.svg" alt="cœur sur toi le temps que ça charge">
-    </section>
-
+        <img
+          src="./assets/images/hearts.svg"
+          alt="cœur sur toi le temps que ça charge"
+        />
+      </section>
     {:then url}
-
-    <section class="screen" id="loader">
+      <section class="screen" id="loader">
         <h2>Youpiiiiiiiiiii !!</h2>
         <p>Le site est publié par ici : <a href={url}>{url}</a></p>
-        <p>Pour rajouter des pages, c'est <a href="./atelier-list-pages">par là</a></p>
-    </section>
-
+        <p>
+          Pour rajouter des pages, c'est <a href="./atelier-list-pages"
+            >par là</a
+          >
+        </p>
+      </section>
     {/await}
-{/if}
-
+  {/if}
+</Skeleton>
 
 <style lang="scss">
-    .url{
-        white-space: nowrap;
-        color: darkslategray;
-    }
+  .url {
+    white-space: nowrap;
+    color: darkslategray;
+  }
 </style>
-
