8c8e0d84e7b35e38a6d01a0aa62237092c30ac5a
Maiana8L <124084791+Maiana8L@users.noreply.github.com>
2023-02-19 20:18:46 +0100

    Ajoute un select theme couleur

--- assets/scripts/components/Settings.svelte
+++ assets/scripts/components/Settings.svelte
@@ -16,6 +16,18 @@
 <Skeleton {publishedWebsiteURL} {buildStatus}>
   <section class="screen" id="settings">
     <h2>L'atelier — Paramètres</h2>
+    <div>
+      <label for="theme-color-select">Choisir un thème couleur :</label>
+      <select>
+        <option value="#2a6442">Vert foncé</option>
+        <option value="#07357d">Bleu foncé</option>
+        <option value="#0E6270">Bleu</option>
+        <option value="#753785">Violet</option>
+        <option value="#993B1F">Rouge foncé</option>
+        <option value="#6C5353">Marron</option>
+        <option value="#53606C">Gris bleuté</option>
+      </select>
+    </div>
     <div class="wrapper delete-zone">
       <label>
         <input
