d034d42f61fc581013c33a863659bc0b57e564af
Maiana8L <124084791+Maiana8L@users.noreply.github.com>
2023-02-19 21:12:52 +0100

    Corrige ma bêtise

--- assets/scripts/components/Settings.svelte
+++ assets/scripts/components/Settings.svelte
@@ -17,17 +17,20 @@
   <section class="screen" id="settings">
     <h2>L'atelier — Paramètres</h2>
     <div class="theme-select">
-      <label for="theme-color-select">Choisir un thème couleur pour mon site :</label>
-      <select class="theme-select-bar">
-        <option value="#2a6442">Vert forêt de sapins</option>
-        <option value="#07357d">Bleu outre-mer</option>
-        <option value="#0E6270">Bleu lagon</option>
-        <option value="#753785">Violet aubergine</option>
-        <option value="#993B1F">Rouge brique</option>
-        <option value="#6C5353">Marron volcanique</option>
-        <option value="#53606C">Gris souris</option>
-      </select>
-      <button class="btn btn__medium">Sauvegarder</button>
+      <div>
+        <label for="theme-color-select">Choisir un thème couleur pour mon site :</label>
+        <select class="theme-select-bar">
+          <option value="#2a6442">Vert forêt de sapins</option>
+          <option value="#07357d">Bleu outre-mer</option>
+          <option value="#0E6270">Bleu lagon</option>
+          <option value="#753785">Violet aubergine</option>
+          <option value="#993B1F">Rouge brique</option>
+          <option value="#6C5353">Marron volcanique</option>
+          <option value="#53606C">Gris souris</option>
+        </select>
+      <div>
+        <button class="btn btn__medium">Sauvegarder</button>
+      </div>
     </div>
     <div class="wrapper delete-zone">
       <label>
