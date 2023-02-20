03ab229e8db2d1bd35561583866cdcfde025b26a
maiwann <27999224+maiwann@users.noreply.github.com>
2023-02-20 20:28:52 +0100

    rajoute consignes pour upload images

--- assets/scripts/makeCreateProjectButtonListener.js
+++ assets/scripts/makeCreateProjectButtonListener.js
@@ -39,8 +39,8 @@ Pour nous contacter : [coucou@lechappeebelle.team](mailto:coucou@lechappeebelle.
 `
 
 const images_readme_content = `
-Dans ce dossier, vous pouvez glisser vos images.\\
-Ensuite, vous pouvez faire un clic droit sur l'image pour récupérer son lien et ensuite l'affiche en Markdown.
+Dans ce dossier, vous pouvez **glisser vos images** ou les ajouter via le bouton "Add file" -> "Upload file" en haut à droite.\\
+Ensuite, vous pouvez faire un clic droit **sur l'image** pour récupérer son lien et ensuite l'afficher en Markdown.
 `
 
 const custom_styles_content = `
