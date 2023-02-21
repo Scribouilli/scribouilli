2cdb2520ae36c6e4c2ab188722fac5da598e2780
Booteille <booteille@protonmail.com>
2023-02-21 14:28:21 +0100

    Fix Website creation description

--- assets/scripts/components/CreateProject.svelte
+++ assets/scripts/components/CreateProject.svelte
@@ -8,8 +8,8 @@
   let projectCreationProgressP = undefined;
 
   function clickListener() {
-    projectCreationProgressP = Promise.resolve(createProject).then(
-      (createProject) => createProject()
+    projectCreationProgressP = Promise.resolve(createProject).then((createProject) =>
+      createProject()
     );
   }
 </script>
@@ -20,12 +20,12 @@
       <h2>Créez un site</h2>
 
       {#await siteRepoConfig}
-        <div><img src="/assets/images/oval.svg" alt=""></div>
-        {:catch}
+        <div><img src="/assets/images/oval.svg" alt="" /></div>
+      {:then}
         {#await publishedWebsiteURL}
           <p>En attente de récupération du nom d'utilisateur·ice</p>
         {:then url}
-          <p>Ce site va être créé à l'adresse <br/> <span class="url">{url}</span></p>
+          <p>Ce site va être créé à l'adresse <br /> <span class="url">{url}</span></p>
         {/await}
       {/await}
 
@@ -36,25 +36,25 @@
       <section class="screen" id="loader">
         <h2>Nous sommes en train de créer le site&nbsp;!</h2>
         <p>Cela peut prendre 2-3 minutes...</p>
-        <img
-          src="./assets/images/hearts.svg"
-          alt="cœur sur toi le temps que ça charge"
-        />
+        <img src="./assets/images/hearts.svg" alt="cœur sur toi le temps que ça charge" />
         <div>
-          <p> Pendant le chargement, vous pouvez&nbsp;: </p>
+          <p>Pendant le chargement, vous pouvez&nbsp;:</p>
           <ul>
-            <li> lire un article pour apprendre à <a href="https://flus.fr/carnet/markdown.html" target="_blank">bidouiller avec du Markdown</a>.</li>
-            <li> ou chanter une petite chanson.
+            <li>
+              lire un article pour apprendre à <a
+                href="https://flus.fr/carnet/markdown.html"
+                target="_blank">bidouiller avec du Markdown</a
+              >.
+            </li>
+            <li>ou chanter une petite chanson.</li>
           </ul>
         </div>
       </section>
     {:then url}
-    <section class="screen" id="loader">
+      <section class="screen" id="loader">
         <h2>Youpiiiiiiiiiii&nbsp;!!</h2>
         <p>Le site est publié par ici&nbsp;: <a href={url} target="_blank">{url}</a></p>
-        <p>
-          Pour éditer les pages, rendez-vous dans l'atelier&nbsp;:
-        </p>
+        <p>Pour éditer les pages, rendez-vous dans l'atelier&nbsp;:</p>
         <a href="./atelier-list-pages" class="btn">Découvrir l'atelier</a>
       </section>
     {/await}
@@ -63,14 +63,14 @@
 
 <style lang="scss">
   #loader {
-  width: 70%;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+    width: 70%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
   p {
-  margin-top: 3rem
+    margin-top: 3rem;
   }
   .url {
     white-space: nowrap;
@@ -78,11 +78,11 @@
   }
   ul {
     margin-top: 1.25rem;
-    text-align: left
+    text-align: left;
   }
   li {
     list-style: disc;
     margin-top: 0.8rem;
-    margin-bottom: 0.8rem
+    margin-bottom: 0.8rem;
   }
 </style>
