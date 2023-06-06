f01a05a1184ab3c5c26361751094e22eb6651233
Ana Gelez <ana@gelez.xyz>
2023-06-06 19:17:24 +0200

    Extraction de la page 'atelier-articles'

--- assets/scripts/routes/main.js
+++ assets/scripts/routes/main.js
@@ -9,6 +9,7 @@ import createProject from "./create-project.js";
 import atelierListArticles from "./atelier-list-articles.js";
 import atelierListPages from "./atelier-list-pages.js";
 import atelierPages from "./atelier-pages.js";
+import atelierArticles from "./atelier-articles.js";
 
 page("/", welcome);
 page("/account", account);
@@ -17,3 +18,4 @@ page("/create-project", createProject);
 page("/atelier-list-articles", atelierListArticles)
 page("/atelier-list-pages", atelierListPages)
 page("/atelier-page", atelierPages)
+page("/atelier-article", atelierArticles)
