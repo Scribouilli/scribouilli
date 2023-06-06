ed7eac172493fc895fe1df0cf7d83271f011ad17
Ana Gelez <ana@gelez.xyz>
2023-06-06 19:00:49 +0200

    Extraction de la page 'atelier-list-pages'

--- assets/scripts/routes/main.js
+++ assets/scripts/routes/main.js
@@ -7,9 +7,11 @@ import account from './account.js';
 import login from './login.js';
 import createProject from "./create-project.js";
 import atelierListArticles from "./atelier-list-articles.js";
+import atelierListPages from "./atelier-list-pages.js";
 
 page("/", welcome);
 page("/account", account);
 page("/login", login);
 page("/create-project", createProject);
 page("/atelier-list-articles", atelierListArticles)
+page("/atelier-list-pages", atelierListPages)
--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -40,42 +40,6 @@ if (url.searchParams.has(TOCTOCTOC_ACCESS_TOKEN_URL_PARAMETER)) {
  * Par ici, y'a des routes
  */
 
-page("/atelier-list-pages", () => {
-  Promise.resolve(store.state.login).then(async (login) => {
-    return checkRepositoryAvailabilityThen(
-      login,
-      store.state.repoName,
-      () => {}
-    );
-  });
-
-  function mapStateToProps(state) {
-    return {
-      publishedWebsiteURL: makePublishedWebsiteURL(state),
-      pages: state.pages,
-      buildStatus: state.buildStatus,
-      repositoryURL: makeRepositoryURL(state),
-    };
-  }
-
-  const state = store.state;
-
-  // @ts-ignore
-  const atelierPages = new AtelierPages({
-    target: svelteTarget,
-    props: mapStateToProps(state),
-  });
-  replaceComponent(atelierPages, mapStateToProps);
-
-  Promise.resolve(state.login).then((login) => {
-    databaseAPI
-      .getPagesList(login, state.repoName)
-      .then((pages) => {
-        store.mutations.setPages(pages);
-      })
-      .catch((msg) => handleErrors(msg));
-  });
-});
 
 page("/atelier-page", ({ querystring }) => {
   Promise.resolve(store.state.login).then(async (login) => {
