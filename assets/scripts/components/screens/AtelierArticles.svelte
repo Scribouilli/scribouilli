0fc49802d704e747f03eb6ba8b9af98bd1e2bb50
E21A950F <claraxpoirier@gmail.com>
2023-06-07 15:31:28 +0200

    création dossier intern pour contenu de l'atelier article

--- assets/scripts/components/screens/AtelierArticles.svelte
+++ assets/scripts/components/screens/AtelierArticles.svelte
@@ -1,49 +1,21 @@
 <script>
-  import Skeleton from "../Skeleton.svelte";
+  import ListContenu from "./intern/ListContenu.svelte";
 
   export let publishedWebsiteURL;
   export let buildStatus;
   export let articles;
   export let repositoryURL;
-
-  console.log(publishedWebsiteURL, repositoryURL);
 </script>
 
-<Skeleton {publishedWebsiteURL} {buildStatus} {repositoryURL}>
-  <section class="screen" id="atelier-list-articles">
-    <div id="articles">
-      <h2>L'atelier — Articles</h2>
-
-      <div class="mesArticles">
-        <ul class="articles-list">
-          {#each (articles || []).sort() as article}
-            <li>
-              <a href="./atelier-article?article={article.path}"
-                >{article.title}</a
-              >
-            </li>
-          {/each}
-        </ul>
-
-        <a href="./atelier-article" class="btn btn__medium">Nouvel article</a>
-      </div>
-    </div>
-  </section>
-</Skeleton>
+<ListContenu
+  {publishedWebsiteURL}
+  {buildStatus}
+  listContenu={articles}
+  {repositoryURL}
+  title="L'atelier — Articles"
+  atelierPrefix="./atelier-article"
+  newContentButtonText="Nouvel article"
+/>
 
 <style lang="scss">
-  .articles-list {
-    margin-bottom: 4rem;
-
-    li {
-      font-size: 1.3rem;
-      & + li {
-        margin-top: 2rem;
-      }
-
-      a {
-        text-transform: capitalize;
-      }
-    }
-  }
 </style>
---
+++ assets/scripts/components/screens/intern/ListContenu.svelte
@@ -0,0 +1,52 @@
+<script>
+  import Skeleton from "../Skeleton.svelte";
+
+  export let publishedWebsiteURL;
+  export let buildStatus;
+  export let listContenu = [];
+  export let repositoryURL;
+  export let title;
+  export let atelierPrefix;
+  export let newContentButtonText;
+</script>
+
+<Skeleton {publishedWebsiteURL} {buildStatus} {repositoryURL}>
+  <section class="screen">
+    <div>
+      <h2>{title}</h2>
+
+      <div>
+        <ul>
+          {#each listContenu.sort() as contenu}
+            <li>
+              <a href="{atelierPrefix}?article={contenu.path}"
+                >{contenu.title}</a
+              >
+            </li>
+          {/each}
+        </ul>
+
+        <a href={atelierPrefix} class="btn btn__medium"
+          >{newContentButtonText}</a
+        >
+      </div>
+    </div>
+  </section>
+</Skeleton>
+
+<style lang="scss">
+  ul {
+    margin-bottom: 4rem;
+
+    li {
+      font-size: 1.3rem;
+      & + li {
+        margin-top: 2rem;
+      }
+
+      a {
+        text-transform: capitalize;
+      }
+    }
+  }
+</style>
