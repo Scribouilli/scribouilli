bcfbba2baf679ff597c1a46806bbbcf7d2421d78
E21A950F <claraxpoirier@gmail.com>
2023-06-07 15:04:36 +0200

    ajout date post article

--- assets/scripts/routes/atelier-articles.js
+++ assets/scripts/routes/atelier-articles.js
@@ -1,13 +1,21 @@
 // @ts-check
 
-import page from 'page'
+import page from "page";
 import parseMarkdown from "@github-docs/frontmatter";
 import store from "../store";
-import { checkRepositoryAvailabilityThen, handleErrors, makeFileNameFromTitle, makeFrontMatterYAMLJsaisPasQuoiLa, makePublishedWebsiteURL, makeRepositoryURL } from "../utils";
+import {
+  checkRepositoryAvailabilityThen,
+  handleErrors,
+  makeFileNameFromTitle,
+  makeFrontMatterYAMLJsaisPasQuoiLa,
+  makePublishedWebsiteURL,
+  makeRepositoryURL,
+} from "../utils";
 import databaseAPI from "../databaseAPI";
 import { svelteTarget } from "../config";
 import { replaceComponent } from "../routeComponentLifeCycle";
 import ArticleContenu from "../components/ArticleContenu.svelte";
+import { format } from "date-fns";
 
 const makeMapStateToProps = (fileName) => (state) => {
   return {
@@ -29,153 +37,150 @@ const makeMapStateToProps = (fileName) => (state) => {
     buildStatus: state.buildStatus,
     repositoryURL: makeRepositoryURL(state),
   };
-}
+};
 
 export default ({ querystring }) => {
-    Promise.resolve(store.state.login).then(async (login) => {
-      return checkRepositoryAvailabilityThen(
-        login,
-        store.state.repoName,
-        () => {}
-      );
-    });
-  
-    const state = store.state;
-    const fileName = new URLSearchParams(querystring).get("article");
-    const mapStateToProps = makeMapStateToProps(fileName)
-  
-    const articleContenu = new ArticleContenu({
-      target: svelteTarget,
-      props: mapStateToProps(store.state),
-    });
-  
-    replaceComponent(articleContenu, mapStateToProps);
-  
-    Promise.resolve(state.login).then((login) => {
-      articleContenu.$set({
-        imageDirUrl: `https://github.com/${login}/${state.repoName}/tree/main/images`,
-      });
-    });
-    articleContenu.$on("delete", ({ detail: { sha } }) => {
-      Promise.resolve(state.login).then((login) => {
-        store.mutations.setArticles(
-          state.articles.filter((article) => {
-            return article.path !== fileName;
-          })
-        );
-        databaseAPI
-          .deleteFile(login, state.repoName, fileName, sha)
-          .then(() => {
-            state.buildStatus.setBuildingAndCheckStatusLater();
-            page("/atelier-list-articles");
-          })
-          .catch((msg) => handleErrors(msg));
-      });
-    });
-  
-    articleContenu.$on(
-      "save",
-      ({
-        detail: { fileName, content, previousContent, title, previousTitle, sha },
-      }) => {
-        const hasContentChanged = content !== previousContent;
-        const hasTitleChanged = title !== previousTitle;
-  
-        // If no content changed, just redirect
-        if (!hasTitleChanged && !hasContentChanged) {
-          page("/atelier-list-articles");
-          return;
-        }
-  
-        let newFileName = fileName;
-        if (fileName !== "index.md") {
-          newFileName = `_posts/${makeFileNameFromTitle(title)}`
-        }
-  
-        const body = {
-          message: `création de l'article ${title || "index.md"}`,
-          content: Buffer.from(
-            `${
-              title ? makeFrontMatterYAMLJsaisPasQuoiLa(title) + "\n" : ""
-            }${content}`
-          ).toString("base64"),
-        };
-  
-        let newArticles =
-          state.articles?.filter((article) => {
-            console.log(article.path, fileName);
-            return article.path !== fileName;
-          }) || [];
-        newArticles.push({ title: title, path: newFileName });
-  
-        store.mutations.setArticles(newArticles);
-  
-        // If title changed
-        if (fileName && fileName !== newFileName) {
-          Promise.resolve(state.login).then((login) => {
-            databaseAPI
-              .updateFile(
-                login,
-                state.repoName,
-                fileName,
-                newFileName,
-                body,
-                sha
-              )
-              .then(() => {
-                if (body.sha) {
-                  console.log("article mise à jour");
-                } else {
-                  console.log("nouvel article créé");
-                }
-                state.buildStatus.setBuildingAndCheckStatusLater();
-                page("/atelier-list-articles");
-              })
-              .catch((msg) => handleErrors(msg));
-          });
-        } else {
-          Promise.resolve(state.login).then((login) => {
-            body.sha = sha;
-            databaseAPI
-              .createFile(login, state.repoName, newFileName, body)
-              .then(() => {
-                if (body.sha) {
-                  console.log("article mise à jour");
-                } else {
-                  console.log("nouvel article créé");
-                }
-                state.buildStatus.setBuildingAndCheckStatusLater();
-                page("/atelier-list-articles");
-              })
-              .catch((msg) => handleErrors(msg));
-          });
-        }
-      }
+  Promise.resolve(store.state.login).then(async (login) => {
+    return checkRepositoryAvailabilityThen(
+      login,
+      store.state.repoName,
+      () => {}
     );
-  
-    // Display existing file
-    if (fileName) {
-      Promise.resolve(store.state.login).then((login) => {
-        databaseAPI
-          .getFile(login, store.state.repoName, fileName)
-          .then(({ content, sha }) => {
-            const contenu = Buffer.from(content, "base64").toString();
-            const {
-              data,
-              content: markdownContent,
-              errors,
-            } = parseMarkdown(contenu);
-  
-            articleContenu.$set({
-              fileName: fileName,
-              content: markdownContent,
-              previousContent: markdownContent,
-              title: data?.title,
-              previousTitle: data?.title,
-              sha: sha,
-            });
-          })
-          .catch((msg) => handleErrors(msg));
-      });
+  });
+
+  const state = store.state;
+  const fileName = new URLSearchParams(querystring).get("article");
+  const mapStateToProps = makeMapStateToProps(fileName);
+
+  const articleContenu = new ArticleContenu({
+    target: svelteTarget,
+    props: mapStateToProps(store.state),
+  });
+
+  replaceComponent(articleContenu, mapStateToProps);
+
+  Promise.resolve(state.login).then((login) => {
+    articleContenu.$set({
+      imageDirUrl: `https://github.com/${login}/${state.repoName}/tree/main/images`,
+    });
+  });
+  articleContenu.$on("delete", ({ detail: { sha } }) => {
+    Promise.resolve(state.login).then((login) => {
+      store.mutations.setArticles(
+        state.articles.filter((article) => {
+          return article.path !== fileName;
+        })
+      );
+      databaseAPI
+        .deleteFile(login, state.repoName, fileName, sha)
+        .then(() => {
+          state.buildStatus.setBuildingAndCheckStatusLater();
+          page("/atelier-list-articles");
+        })
+        .catch((msg) => handleErrors(msg));
+    });
+  });
+
+  articleContenu.$on(
+    "save",
+    ({
+      detail: { fileName, content, previousContent, title, previousTitle, sha },
+    }) => {
+      const hasContentChanged = content !== previousContent;
+      const hasTitleChanged = title !== previousTitle;
+
+      // If no content changed, just redirect
+      if (!hasTitleChanged && !hasContentChanged) {
+        page("/atelier-list-articles");
+        return;
+      }
+
+      let newFileName = fileName;
+      if (fileName !== "index.md") {
+        const date = new Date();
+        newFileName = `_posts/${format(
+          date,
+          "yyyy-MM-dd"
+        )}-${makeFileNameFromTitle(title)}`;
+      }
+
+      const body = {
+        message: `création de l'article ${title || "index.md"}`,
+        content: Buffer.from(
+          `${
+            title ? makeFrontMatterYAMLJsaisPasQuoiLa(title) + "\n" : ""
+          }${content}`
+        ).toString("base64"),
+      };
+
+      let newArticles =
+        state.articles?.filter((article) => {
+          console.log(article.path, fileName);
+          return article.path !== fileName;
+        }) || [];
+      newArticles.push({ title: title, path: newFileName });
+
+      store.mutations.setArticles(newArticles);
+
+      // If title changed
+      if (fileName && fileName !== newFileName) {
+        Promise.resolve(state.login).then((login) => {
+          databaseAPI
+            .updateFile(login, state.repoName, fileName, newFileName, body, sha)
+            .then(() => {
+              if (body.sha) {
+                console.log("article mise à jour");
+              } else {
+                console.log("nouvel article créé");
+              }
+              state.buildStatus.setBuildingAndCheckStatusLater();
+              page("/atelier-list-articles");
+            })
+            .catch((msg) => handleErrors(msg));
+        });
+      } else {
+        Promise.resolve(state.login).then((login) => {
+          body.sha = sha;
+          databaseAPI
+            .createFile(login, state.repoName, newFileName, body)
+            .then(() => {
+              if (body.sha) {
+                console.log("article mise à jour");
+              } else {
+                console.log("nouvel article créé");
+              }
+              state.buildStatus.setBuildingAndCheckStatusLater();
+              page("/atelier-list-articles");
+            })
+            .catch((msg) => handleErrors(msg));
+        });
+      }
     }
-}
+  );
+
+  // Display existing file
+  if (fileName) {
+    Promise.resolve(store.state.login).then((login) => {
+      databaseAPI
+        .getFile(login, store.state.repoName, fileName)
+        .then(({ content, sha }) => {
+          const contenu = Buffer.from(content, "base64").toString();
+          const {
+            data,
+            content: markdownContent,
+            errors,
+          } = parseMarkdown(contenu);
+
+          articleContenu.$set({
+            fileName: fileName,
+            content: markdownContent,
+            previousContent: markdownContent,
+            title: data?.title,
+            previousTitle: data?.title,
+            sha: sha,
+          });
+        })
+        .catch((msg) => handleErrors(msg));
+    });
+  }
+};
