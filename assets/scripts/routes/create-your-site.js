d9dfdf65c52070374c4f7617d9ba5ece73190cef
Fanny Cheung <dev@ynote.hk>
2023-06-20 16:39:44 +0200

    Renomme encore le screen pour choisir de creer ou utiliser un site

--- assets/scripts/routes/create-your-site.js
+++ assets/scripts/routes/create-your-site.js
@@ -3,13 +3,13 @@ import page from 'page'
 import { svelteTarget } from "../config";
 import { replaceComponent } from "../routeComponentLifeCycle";
 import store from "../store";
-import SelectASite from '../components/SelectASite.svelte'
+import SelectOrCreateSite from '../components/SelectOrCreateSite.svelte'
 
 export default () => {
-    const selectASite = new SelectASite({
+    const selectOrCreateSite = new SelectOrCreateSite({
       target: svelteTarget,
       props: {},
     });
 
-    replaceComponent(selectASite, () => {});
+    replaceComponent(selectOrCreateSite, () => {});
 }
