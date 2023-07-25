caa4c1669530a20270183444c70378e8f01723a6
E21A950F <claraxpoirier@gmail.com>
2023-07-25 16:30:41 +0200

    accès à l'atelier uniquement quand le dépôt est prêt

--- assets/scripts/databaseAPI.js
+++ assets/scripts/databaseAPI.js
@@ -5,8 +5,7 @@ import FS from '@isomorphic-git/lightning-fs'
 import git from 'isomorphic-git'
 import http from 'isomorphic-git/http/web'
 
-import store from "./store.js";
-
+import store from './store.js'
 
 const CORS_PROXY_URL = 'https://cors.isomorphic-git.org'
 
@@ -16,55 +15,51 @@ class DatabaseAPI {
    */
   constructor(accessToken) {
     /** @type {string | undefined} */
-    this.accessToken = accessToken;
-    this.commitsEtag = undefined;
-    this.latestCommit = undefined;
-    this.getFilesCache = new Map();
-    this.fileCached = undefined;
-    this.customCSSPath = "assets/css/custom.css";
-    this.defaultRepoOwner = "Scribouilli"
-    this.defaultThemeRepoName = "site-template"
-    this.fs = new FS(
-      'scribouilli',
-      // @ts-ignore il y a un problème avec la définition de type dans lightning-fs, db est un paramètre optionnel
-      { wipe: true }
-    )
+    this.accessToken = accessToken
+    this.commitsEtag = undefined
+    this.latestCommit = undefined
+    this.getFilesCache = new Map()
+    this.fileCached = undefined
+    this.customCSSPath = 'assets/css/custom.css'
+    this.defaultRepoOwner = 'Scribouilli'
+    this.defaultThemeRepoName = 'site-template'
+    this.fs = new FS('scribouilli')
   }
 
   getAuthenticatedUser() {
-    return this.callGithubAPI("https://api.github.com/user").then(
-      (response) => {
-        return response.json();
-      }
-    );
+    return this.callGithubAPI('https://api.github.com/user').then(response => {
+      return response.json()
+    })
   }
 
   getUserEmails() {
-    return this.callGithubAPI('https://api.github.com/user/emails').then(r => r.json())
+    return this.callGithubAPI('https://api.github.com/user/emails').then(r =>
+      r.json(),
+    )
   }
 
   getRepository(login, repoName) {
     return this.callGithubAPI(
-      `https://api.github.com/repos/${login}/${repoName}`
+      `https://api.github.com/repos/${login}/${repoName}`,
     )
-      .then((response) => {
-        return response.json();
+      .then(response => {
+        return response.json()
       })
-      .catch((msg) => {
-        if (msg === "NOT_FOUND") {
-          throw "REPOSITORY_NOT_FOUND";
+      .catch(msg => {
+        if (msg === 'NOT_FOUND') {
+          throw 'REPOSITORY_NOT_FOUND'
         }
 
-        throw msg;
-      });
+        throw msg
+      })
   }
 
   getCurrentUserRepositories() {
     return this.callGithubAPI(
-      `https://api.github.com/user/repos?sort=updated&affiliation=owner&visibility=public`
-    ).then((response) => {
-      return response.json();
-    });
+      `https://api.github.com/user/repos?sort=updated&affiliation=owner&visibility=public`,
+    ).then(response => {
+      return response.json()
+    })
   }
 
   async createDefaultRepository(login, newRepoName) {
@@ -72,14 +67,14 @@ class DatabaseAPI {
       `https://api.github.com/repos/${this.defaultRepoOwner}/${this.defaultThemeRepoName}/generate`,
       {
         headers: {
-          Authorization: "token " + this.accessToken,
-          Accept: "application/vnd.github+json",
+          Authorization: 'token ' + this.accessToken,
+          Accept: 'application/vnd.github+json',
         },
-        method: "POST",
+        method: 'POST',
         body: JSON.stringify({
           owner: login,
           name: newRepoName,
-          description: "Mon site Scribouilli",
+          description: 'Mon site Scribouilli',
         }),
       }
     );
@@ -116,8 +111,8 @@ class DatabaseAPI {
       `https://api.github.com/repos/${account}/${repoName}/pages`,
       {
         headers: {
-          Authorization: "token " + this.accessToken,
-          Accept: "applicatikn/vnd.github+json",
+          Authorization: 'token ' + this.accessToken,
+          Accept: 'applicatikn/vnd.github+json',
         },
         method: "POST",
         body: JSON.stringify({ source: { branch: "main" } }),
@@ -126,23 +121,27 @@ class DatabaseAPI {
   }
 
   /**
-   * 
-   * @param {string} login 
-   * @param {string} repoName 
+   *
+   * @param {string} login
+   * @param {string} repoName
    * @returns {string}
    */
   repoDir(login, repoName) {
-    if(typeof login !== 'string'){
-      throw new Error(`login is not a string (${typeof login} - ${Object(login) === login ? login[Symbol.toStringTag] : ''})`)
+    if (typeof login !== 'string') {
+      throw new Error(
+        `login is not a string (${typeof login} - ${
+          Object(login) === login ? login[Symbol.toStringTag] : ''
+        })`,
+      )
     }
 
     return `/github.com/${login}/${repoName}`
   }
 
   /**
-   * 
-   * @param {string} login 
-   * @param {string} repoName 
+   *
+   * @param {string} login
+   * @param {string} repoName
    * @param {string} fileName
    * @returns {string}
    */
@@ -151,9 +150,9 @@ class DatabaseAPI {
   }
 
   /**
-   * 
-   * @param {string} login 
-   * @param {string} repoName 
+   *
+   * @param {string} login
+   * @param {string} repoName
    * @returns {Promise<void>}
    */
   async cloneIfNeeded(login, repoName) {
@@ -175,25 +174,25 @@ class DatabaseAPI {
         corsProxy: CORS_PROXY_URL,
         ref: 'main',
         singleBranch: true,
-        depth: 5
+        depth: 5,
       })
     }
   }
 
   /**
    * Assigne l'auteur et l'email pour les commits git
-   * 
+   *
    * On voudrait le faire en global, mais ça n'est pas possible actuellement avec isomorphic-git (1.24.2)
    * > Currently only the local $GIT_DIR/config file can be read or written. However support for the global ~/.gitconfig and system $(prefix)/etc/gitconfig will be added in the future.
-   * Voir https://github.com/isomorphic-git/isomorphic-git/pull/1779 
-   * 
+   * Voir https://github.com/isomorphic-git/isomorphic-git/pull/1779
+   *
    *
    * https://isomorphic-git.org/docs/en/setConfig
-   * 
+   *
    * Alors, on doit passer le repoName
-   * 
-   * @param {string} login 
-   * @param {string} repoName 
+   *
+   * @param {string} login
+   * @param {string} repoName
    * @param {string} email
    * @returns {Promise<void>}
    */
@@ -229,7 +228,10 @@ class DatabaseAPI {
    */
   async getFile(login, repoName, fileName) {
     await this.cloneIfNeeded(login, repoName)
-    const content = await this.fs.promises.readFile(this.path(login, repoName, fileName), { encoding: 'utf8' })
+    const content = await this.fs.promises.readFile(
+      this.path(login, repoName, fileName),
+      { encoding: 'utf8' },
+    )
     if (content instanceof Uint8Array) {
       return content.toString()
     }
@@ -238,16 +240,16 @@ class DatabaseAPI {
 
   getGitHubPagesSite(login, repoName) {
     return this.callGithubAPI(
-      `https://api.github.com/repos/${login}/${repoName}/pages`
-    ).then((response) => {
-      return response.json();
-    });
+      `https://api.github.com/repos/${login}/${repoName}/pages`,
+    ).then(response => {
+      return response.json()
+    })
   }
 
   getDeploymentStatus(deployment) {
-    return this.callGithubAPI(deployment.statuses_url).then((response) => {
-      return response.json();
-    });
+    return this.callGithubAPI(deployment.statuses_url).then(response => {
+      return response.json()
+    })
   }
 
   async push(login, repoName) {
@@ -262,15 +264,15 @@ class DatabaseAPI {
           username: this.accessToken,
           password: 'x-oauth-basic',
         }
-      }
+      },
     })
   }
 
   /**
    * @summary Remove file from github
-   * 
-   * @param {string} login 
-   * @param {string} repoName 
+   *
+   * @param {string} login
+   * @param {string} repoName
    * @param {string} fileName
    * @returns {Promise<void>}
    */
@@ -287,7 +289,7 @@ class DatabaseAPI {
     await git.commit({
       fs: this.fs,
       dir: this.repoDir(login, repoName),
-      message: `suppression du fichier ${fileName}`
+      message: `suppression du fichier ${fileName}`,
     })
     this.push(login, repoName)
   }
@@ -297,10 +299,10 @@ class DatabaseAPI {
     return await this.callGithubAPI(
       `https://api.github.com/repos/${login}/${repoName}`,
       {
-        headers: { Authorization: "token " + this.accessToken },
-        method: "DELETE",
-      }
-    );
+        headers: { Authorization: 'token ' + this.accessToken },
+        method: 'DELETE',
+      },
+    )
   }
 
   /**
@@ -311,7 +313,7 @@ class DatabaseAPI {
    * @param {string} content
    * @param {string} message
    * @param {string | { old: string, new: string }} fileName
-   * 
+   *
    * @returns {Promise<void>}
    */
   async writeFile(login, repoName, fileName, content, message) {
@@ -329,46 +331,71 @@ class DatabaseAPI {
       throw new TypeError('Empty file name')
     }
 
-    await this.fs.promises.writeFile(this.path(login, repoName, fileName), content)
-    await git.add({ fs: this.fs, filepath: fileName, dir: this.repoDir(login, repoName) })
-    await git.commit({ fs: this.fs, dir: this.repoDir(login, repoName), message })
+    await this.fs.promises.writeFile(
+      this.path(login, repoName, fileName),
+      content,
+    )
+    await git.add({
+      fs: this.fs,
+      filepath: fileName,
+      dir: this.repoDir(login, repoName),
+    })
+    await git.commit({
+      fs: this.fs,
+      dir: this.repoDir(login, repoName),
+      message,
+    })
     this.push(login, repoName)
   }
 
   async writeCustomCSS(login, repoName, content) {
-    return this.writeFile(login, repoName, this.customCSSPath, content, "mise à jour du ficher de styles custom");
+    return this.writeFile(
+      login,
+      repoName,
+      this.customCSSPath,
+      content,
+      'mise à jour du ficher de styles custom',
+    )
   }
 
   /**
-   * 
-   * @param {string} login 
-   * @param {string} repoName 
+   *
+   * @param {string} login
+   * @param {string} repoName
    * @param {string} dir
-   * @returns 
+   * @returns
    */
   async getPagesList(login, repoName, dir = '') {
     await this.cloneIfNeeded(login, repoName)
 
-    const allFiles = await this.fs.promises.readdir(this.path(login, repoName, dir))
-    return Promise.all(allFiles
-      .filter(f => f.endsWith(".md") || f.endsWith(".html"))
-      .map(async f => {
-        const path = dir == '' ? f : `${dir}/${f}`
-        const content = await this.fs.promises.readFile(this.path(login, repoName, path))
-        const { attributes: data, body: markdownContent } = lireFrontMatter(content.toString());
-        return {
-          title: data?.title,
-          path,
-          content: markdownContent,
-        };
-      }))
+    const allFiles = await this.fs.promises.readdir(
+      this.path(login, repoName, dir),
+    )
+    return Promise.all(
+      allFiles
+        .filter(f => f.endsWith('.md') || f.endsWith('.html'))
+        .map(async f => {
+          const path = dir == '' ? f : `${dir}/${f}`
+          const content = await this.fs.promises.readFile(
+            this.path(login, repoName, path),
+          )
+          const { attributes: data, body: markdownContent } = lireFrontMatter(
+            content.toString(),
+          )
+          return {
+            title: data?.title,
+            path,
+            content: markdownContent,
+          }
+        }),
+    )
   }
 
   /**
-   * 
-   * @param {string} login 
-   * @param {string} repoName 
-   * @returns 
+   *
+   * @param {string} login
+   * @param {string} repoName
+   * @returns
    */
   async getArticlesList(login, repoName) {
     try {
@@ -380,21 +407,32 @@ class DatabaseAPI {
 
   getLastDeployment(login, repoName) {
     return this.callGithubAPI(
-      `https://api.github.com/repos/${login}/${repoName}/deployments?per_page=1`
-    ).then((deployments) => deployments[0]);
+      `https://api.github.com/repos/${login}/${repoName}/deployments?per_page=1`,
+    ).then(deployments => deployments[0])
   }
 
   /**
-   * 
-   * @param {string} login 
-   * @param {string} repoName 
-   * @param {string} path 
-   * @returns 
+   *
+   * @param {string} login
+   * @param {string} repoName
+   * @param {string} path
+   * @returns
    */
   async checkFileExistence(login, repoName, path) {
     await this.cloneIfNeeded(login, repoName)
     const stat = await this.fs.promises.stat(this.path(login, repoName, path))
     return stat.isFile()
+  }
+
+  async checkRepoReady(login, repoName) {
+    try {
+      const appel = await this.callGithubAPI(
+        `https://api.github.com/repos/${login}/${repoName}/contents/_config.yml`,
+      )
+      return appel.ok
+    } catch (error) {
+      return false
+    }
   }
 
   /**
@@ -405,31 +443,31 @@ class DatabaseAPI {
    */
   callGithubAPI(
     url,
-    requestParams = { headers: { Authorization: "token " + this.accessToken } }
+    requestParams = { headers: { Authorization: 'token ' + this.accessToken } },
   ) {
-    return fetch(url, requestParams).then((httpResp) => {
+    return fetch(url, requestParams).then(httpResp => {
       if (httpResp.status === 404) {
-        throw "NOT_FOUND";
+        throw 'NOT_FOUND'
       }
 
       if (httpResp.status === 401) {
-        this.accessToken = undefined;
-        console.debug("this accessToken : ", this);
-        throw "INVALIDATE_TOKEN";
+        this.accessToken = undefined
+        console.debug('this accessToken : ', this)
+        throw 'INVALIDATE_TOKEN'
       }
-      return httpResp;
-    });
+      return httpResp
+    })
   }
 }
 
 /** @type {DatabaseAPI} */
-let databaseAPI = new DatabaseAPI('');
+let databaseAPI = new DatabaseAPI('')
 
 // Create the databaseAPI singleton with the logged-in user access token.
 if (store.state.accessToken) {
-  databaseAPI = new DatabaseAPI(store.state.accessToken);
+  databaseAPI = new DatabaseAPI(store.state.accessToken)
 } else {
-  history.replaceState(undefined, "", store.state.basePath + "/");
+  history.replaceState(undefined, '', store.state.basePath + '/')
 }
 
-export default databaseAPI;
+export default databaseAPI
