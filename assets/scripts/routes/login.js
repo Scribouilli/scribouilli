96681f48afe3b3d59c317736493367cc10e950ef
Ana Gelez <ana@gelez.xyz>
2023-07-26 16:31:48 +0200

    TTT EB

--- assets/scripts/routes/login.js
+++ assets/scripts/routes/login.js
@@ -7,8 +7,8 @@ import Login from "../components/screens/Login.svelte";
 
 export default () => {
   const destination = location.origin + store.state.basePath + "/after-github-login";
-  const client_id = "64ecce0b01397c2499a6";
-  const redirect_url = "https://toctoctoc.dreads-unlock.fr/github-callback";
+  const client_id = "3b7ce36803222bdfe233";
+  const redirect_url = "https://toctoctoc.lechappeebelle.team/github-callback";
   const githubLoginHref = `https://github.com/login/oauth/authorize?client_id=${client_id}&scope=public_repo,user:email&redirect_uri=${redirect_url}?destination=${destination}`;
 
   const login = new Login({
