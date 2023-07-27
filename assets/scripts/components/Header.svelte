e631be4c228b5859c1f4b95372c89a96540dc66b
E21A950F <claraxpoirier@gmail.com>
2023-07-27 11:59:50 +0200

    stylification du header

--- assets/scripts/components/Header.svelte
+++ assets/scripts/components/Header.svelte
@@ -1,22 +1,22 @@
 <script>
   // @ts-check
-  export let buildStatus;
-  export let currentRepository;
+  export let buildStatus
+  export let currentRepository
   export let showArticles
 
   let status
 
-  $: status = buildStatus?.status;
+  $: status = buildStatus?.status
 
   if (buildStatus) {
-    buildStatus.subscribe((s) => {
+    buildStatus.subscribe(s => {
       if (s) {
-        status = s;
+        status = s
       }
-    });
+    })
   }
 
-  $: buildStatusClass = buildStatus ? `build-${status}` : undefined;
+  $: buildStatusClass = buildStatus ? `build-${status}` : undefined
 
   let publishedWebsiteURL
   $: publishedWebsiteURL = currentRepository?.publishedWebsiteURL
@@ -27,74 +27,89 @@
   let account
   $: account = currentRepository?.owner
 
-  $: homeURL = repoName && account ? 
-    `./atelier-list-pages?repoName=${repoName}&account=${account}` :
-    './'
-
+  $: homeURL =
+    repoName && account
+      ? `./atelier-list-pages?repoName=${repoName}&account=${account}`
+      : './'
 </script>
 
 <header>
-  <h1><a href="{homeURL}" class="go-home">Scribouilli</a></h1>
-
   {#if publishedWebsiteURL}
     <div>
-      <div>
-        <a href="https://{publishedWebsiteURL}" class="project-name" target="_blank">
+      <p>
+        <a
+          href="https://{publishedWebsiteURL}"
+          class="project-name"
+          target="_blank"
+        >
           {publishedWebsiteURL}
         </a>
-        {#if buildStatusClass}
-          <p class={buildStatusClass} />
-        {/if}
-      </div>
+      </p>
+      {#if buildStatusClass}
+        <p class={buildStatusClass} />
+      {/if}
+    </div>
+  {/if}
 
-      <nav>
-        <ul>
-          <li>
-            <a href="./atelier-list-pages?repoName={repoName}&account={account}">
-              Pages
-            </a>
-          </li>
+  <h1>
+    <a href={homeURL} class="go-home"
+      ><img
+        src="./assets/images/logo_atelier.png"
+        alt="L'atelier de Scribouilli"
+      /></a
+    >
+  </h1>
 
-          {#if showArticles}
+  {#if publishedWebsiteURL}
+    <nav>
+      <ul>
+        <li>
+          <a href="./atelier-list-pages?repoName={repoName}&account={account}">
+            Pages
+          </a>
+        </li>
+
+        {#if showArticles}
           <li>
-            <a href="./atelier-list-articles?repoName={repoName}&account={account}">
+            <a
+              href="./atelier-list-articles?repoName={repoName}&account={account}"
+            >
               Articles
             </a>
           </li>
-          {/if}
+        {/if}
 
-          <li>
-            <a href="./settings?repoName={repoName}&account={account}">
-              Paramètres
-            </a>
-          </li>
-          <li>
-            {#if repositoryURL}
-              {#await repositoryURL then urlrepository}
-                <a href={urlrepository} target="_blank">GitHub</a>
-              {/await}
-            {/if}
-          </li>
-        </ul>
-      </nav>
-    </div>
+        <li>
+          <a href="./settings?repoName={repoName}&account={account}">
+            Paramètres
+          </a>
+        </li>
+        <li>
+          {#if repositoryURL}
+            {#await repositoryURL then urlrepository}
+              <a href={urlrepository} target="_blank">GitHub</a>
+            {/await}
+          {/if}
+        </li>
+      </ul>
+    </nav>
   {/if}
 </header>
 
 <style lang="scss">
-  [class^="build-"] {
+  [class^='build-'] {
     margin-top: 0.3rem;
   }
 
   .build-building::after {
-    content: "🕰 En cours de publication (2-3 min)";
+    content: '🕰 En cours de publication (2-3 min)';
   }
 
   .build-built::after {
-    content: "✅ Site à jour";
+    content: '✅ Site à jour';
   }
 
   .build-errored::after {
-    content: "🕰 En cours de publication (15 min max)";
+    content: '🕰 En cours de publication (15 min max)';
   }
 </style>
