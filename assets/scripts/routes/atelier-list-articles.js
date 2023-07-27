27068dfb0046d9af45f720a54c281a19d2a8741f
E21A950F <claraxpoirier@gmail.com>
2023-07-27 10:48:24 +0200

    fix retour éditeur

--- assets/scripts/routes/atelier-list-articles.js
+++ assets/scripts/routes/atelier-list-articles.js
@@ -1,43 +1,45 @@
 // @ts-check
 
-import AtelierArticles from "../components/screens/AtelierArticles.svelte";
-import { svelteTarget } from "../config";
-import { replaceComponent } from "../routeComponentLifeCycle";
-import store from "../store";
+import AtelierArticles from '../components/screens/AtelierArticles.svelte'
+import { svelteTarget } from '../config'
+import { replaceComponent } from '../routeComponentLifeCycle'
+import store from '../store'
 import {
   getCurrentRepoArticles,
   setCurrentRepositoryFromQuerystring,
-} from "../actions";
+} from '../actions'
 
 function mapStateToProps(state) {
   return {
     articles: state.articles,
     buildStatus: state.buildStatus,
     currentRepository: state.currentRepository,
-    showArticles: state.pages.find(p => p.path === 'blog.md') !== undefined || state.articles?.length > 0,
-  };
+    showArticles:
+      state.pages.find(p => p.path === 'blog.md') !== undefined ||
+      state.articles?.length > 0,
+  }
 }
 
 export default ({ querystring }) => {
-  setCurrentRepositoryFromQuerystring(querystring);
+  setCurrentRepositoryFromQuerystring(querystring)
 
-  const state = store.state;
+  const state = store.state
   const atelierArticles = new AtelierArticles({
     target: svelteTarget,
     props: mapStateToProps(state),
-  });
+  })
 
-  replaceComponent(atelierArticles, mapStateToProps);
+  replaceComponent(atelierArticles, mapStateToProps)
 
-  Promise.resolve(state.login).then(() => getCurrentRepoArticles());
+  Promise.resolve(state.login).then(() => getCurrentRepoArticles())
 }
 
 /**
- * 
- * @param {string} account 
- * @param {string} repoName 
+ *
+ * @param {string} account
+ * @param {string} repoName
  * @returns {string}
  */
-export function makeAtelierListArticlesURL(account, repoName){
-  return `/atelier-list-articles?account=${account}&repoName=${repoName}`
+export function makeAtelierListArticlesURL(account, repoName) {
+  return `./atelier-list-articles?account=${account}&repoName=${repoName}`
 }
--- assets/scripts/routes/atelier-list-pages.js
+++ assets/scripts/routes/atelier-list-pages.js
@@ -1,47 +1,48 @@
 // @ts-check
 
-import { svelteTarget } from "../config";
-import { replaceComponent } from "../routeComponentLifeCycle";
-import store from "../store";
+import { svelteTarget } from '../config'
+import { replaceComponent } from '../routeComponentLifeCycle'
+import store from '../store'
 import {
   getCurrentRepoPages,
   setCurrentRepositoryFromQuerystring,
-} from "../actions";
-import AtelierPages from "../components/screens/AtelierPages.svelte";
+} from '../actions'
+import AtelierPages from '../components/screens/AtelierPages.svelte'
 
 /**
- * 
- * @param {import("../store").ScribouilliState} state 
- * @returns 
+ *
+ * @param {import("../store").ScribouilliState} state
+ * @returns
  */
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     pages: state.pages.filter(p => p.path !== 'blog.md'),
     buildStatus: state.buildStatus,
     currentRepository: state.currentRepository,
-    showArticles: state.pages.find(p => p.path === 'blog.md') !== undefined || state.articles?.length > 0,
-  };
+    showArticles:
+      state.pages.find(p => p.path === 'blog.md') !== undefined ||
+      state.articles?.length > 0,
+  }
 }
 
 export default async ({ querystring }) => {
-  setCurrentRepositoryFromQuerystring(querystring)
-    .then(getCurrentRepoPages);
+  setCurrentRepositoryFromQuerystring(querystring).then(getCurrentRepoPages)
 
-  const state = store.state;
+  const state = store.state
   const atelierPages = new AtelierPages({
     target: svelteTarget,
     props: mapStateToProps(state),
-  });
+  })
 
-  replaceComponent(atelierPages, mapStateToProps);
+  replaceComponent(atelierPages, mapStateToProps)
 }
 
 /**
- * 
- * @param {string} account 
- * @param {string} repoName 
+ *
+ * @param {string} account
+ * @param {string} repoName
  * @returns {string}
  */
-export function makeAtelierListPageURL(account, repoName){
-  return `/atelier-list-pages?account=${account}&repoName=${repoName}`
+export function makeAtelierListPageURL(account, repoName) {
+  return `./atelier-list-pages?account=${account}&repoName=${repoName}`
 }
