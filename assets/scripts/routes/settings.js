e308dd00381f9d60cddc038b2e0a18a7de1ffe6e
Gelez <ana@gelez.xyz>
2023-07-28 15:44:38 +0200

    Ajoute un lien vers le flux RSS (#87)

--- assets/scripts/routes/settings.js
+++ assets/scripts/routes/settings.js
@@ -21,6 +21,11 @@ permalink: /articles/
 <h1>
   Articles
 </h1>
+<aside>
+  <a href="{{ '/feed.xml' | relative_url }}">S'abonner via le flux RSS</a>
+  &mdash;
+  <a href="https://flus.fr/carnet/a-quoi-servent-les-flux.html">C'est quoi ?</a>
+</aside>
 
 {% for post in site.posts %}
 <article class="blog-item">
