c66dfc8256d5ae17d99177d710de7280f5c06edc
newick <erick@entre-quote.com>
2023-09-08 16:06:49 +0200

    change l'url du site explicatif dans le footer

--- assets/scripts/components/Footer.svelte
+++ assets/scripts/components/Footer.svelte
@@ -1,6 +1,6 @@
 <footer>
   <div>
-    <a href="https://scribouilli.lechappeebelle.team/" target="_blank">
+    <a href="https://scribouilli.org/" target="_blank">
       Site explicatif Scribouilli
     </a>
   </div>
