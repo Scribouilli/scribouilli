ad97c64f142a672508a87caa5690195ef02097b0
David Bruant <davidbruant@protonmail.com>
2023-10-27 10:50:51 +0200

    Rajout de types minimaux de l'API github

---
+++ assets/scripts/types.js
@@ -0,0 +1,11 @@
+/**
+ * @typedef {Object} GithubUserEmails
+ * @property {string} email
+ * @property {boolean} primary
+ */
+
+/**
+ * @typedef {Object} GithubDeployment
+ * @property {string} statuses_url
+ */
+
