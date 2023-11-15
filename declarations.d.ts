26e3df9ccc53697082b484572960e7597a2bf62d
Fanny Cheung <dev@ynote.hk>
2023-11-15 12:10:21 +0100

    Add unit tests (#111)

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,1 +1,3 @@
 declare module 'marked'
+declare const expect: Chai.ExpectStatic
+declare const sinon: sinon.SinonStatic
---
+++ tests/setup.js
@@ -0,0 +1,31 @@
+// @ts-nocheck
+
+import 'mock-local-storage'
+import 'fake-indexeddb/auto'
+
+import { JSDOM } from 'jsdom'
+import chai, { expect } from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+
+import 'chai/register-expect.js'
+
+// Setup chai and sinon
+global.sinon = sinon
+global.chai = chai
+chai.use(sinonChai)
+
+// Use a fake DOM for testing
+const dom = new JSDOM(``, {
+  url: 'https://example.com/',
+  referrer: 'https://example.com/',
+  contentType: 'text/html',
+  includeNodeLocations: true,
+  storageQuota: 10000000,
+})
+
+global.document = dom.window.document
+global.window = dom.window
+global.location = dom.window.location
+global.navigator = dom.window.navigator
+global.history = dom.window.history
