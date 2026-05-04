209783a846e21ea2647dd0ddc1e5e30827d4b89a
newick <erick@entre-quote.com>
2026-05-04 11:02:10 +0200

    Avance dans le process de connexion du test e2e

--- tests/e2e/test.spec.ts
+++ tests/e2e/test.spec.ts
@@ -1,5 +1,8 @@
 import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv';
+import path from 'path';
 
+dotenv.config({ path: path.resolve(import.meta.dirname, '.env') });
 
 test('Je crée un minisite', async ({ page }) => {
   await page.goto('https://atelier.scribouilli.org/');
@@ -19,4 +22,13 @@ test('Je crée un minisite', async ({ page }) => {
   // Clés
   await expect(page.getByRole('heading', { name: /Super/i })).toBeVisible();
   await page.getByRole('link', { name: /Je me connecte/i }).click();
+
+  // Scribougit
+  await expect(page).toHaveURL('https://git.scribouilli.org/users/sign_in');
+  
+  // ici ça ne passe pas, peut-être que le label est en anglais
+  await page.getByLabel(`Nom d'utilisateur ou adresse de courriel principale`).fill(process.env["scribougit_username"]);
+  await page.getByLabel(`Mot de passe`).fill(process.env["scribougit_password"]);
+  await page.getByRole('link', { name: 'Connexion' }).click();
+  await expect(page).toHaveURL(/^https\:\/\/git\.scribouilli\.org\/oauth\/authorize/);
 });
